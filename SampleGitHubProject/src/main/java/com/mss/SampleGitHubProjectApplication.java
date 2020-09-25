a021d1b89db9844c71c65170a3b443e634bdb070
kmallavarapu <kmallavarapu@MC90.corp.miraclesoft.com>
2020-09-25 15:51:39 +0530

    Initial Commit

---
+++ SampleGitHubProject/src/main/java/com/mss/SampleGitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGitHubProjectApplication.class, args);
+	}
+
+}
